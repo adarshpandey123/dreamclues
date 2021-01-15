1c98e841a094c12f266de66b0c442a75117de15e
adarsh pandey <adarshpandey7903@gmail.com>
2021-01-16 03:28:57 +0530

    react-project

---
+++ our-team.php
@@ -0,0 +1,93 @@
+<section class="team spad">
+<div class="container">
+<div class="row">
+<div class="col-lg-9 col-md-8 col-sm-6">
+<div class="section-title">
+<span>Our Team</span>
+<h2>Meet our team</h2>
+</div>
+</div>
+
+<div class="col-lg-3 col-md-4 col-sm-6">
+
+</div>
+</div>
+<div class="row">
+
+        
+<div class="col-lg-3 col-md-6">
+
+<div class="team__item set-bg" data-setbg="img/team/TEAM1.jpeg">
+<div class="team__text">
+<div class="team__title">
+<h5 class="text-light">Md Sabahuzzaman</h5>
+<span class="text-light">Founder & Director</span>
+</div>
+<p>MBA,University School of Management Studies,GGSIPU</p>
+<div class="team__social">
+
+<a href="#"><i class="fa fa-linkedin"></i></a>
+
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-3 col-md-6">
+
+<div class="team__item set-bg" data-setbg="img/team/TEAM2.jpeg">
+<div class="team__text">
+<div class="team__title">
+<h5 class="text-light">Nouman Alam</h5>
+<span class="text-light">Founder & CEO</span>
+ </div>
+<p>Psychology(hons),Department of Psychology,Jamia Millia Islamia.</p>
+<div class="team__social">
+
+<a href="#"><i class="fa fa-linkedin"></i></a>
+
+</div>
+</div>
+</div>
+</div>
+
+<div class="col-lg-3 col-md-6">
+
+<div class="team__item set-bg" data-setbg="img/team/TEAM2.jpeg">
+<div class="team__text">
+<div class="team__title">
+<h5 class="text-light">Nouman Alam</h5>
+<span class="text-light">Founder & CEO</span>
+ </div>
+<p>Psychology(hons),Department of Psychology,Jamia Millia Islamia.</p>
+<div class="team__social">
+
+<a href="#"><i class="fa fa-linkedin"></i></a>
+
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-3 col-md-6">
+
+<div class="team__item set-bg" data-setbg="img/team/TEAM4.jpeg">
+<div class="team__text">
+<div class="team__title">
+<h5 class="text-light">Syed Danish Ahmad</h5>
+<span class="text-light">Business Development Executive </span>
+</div>
+<p>B.tech Mechanical(Jawaharlal Tecnical University,Hydrabad)</p>
+<div class="team__social">
+
+<a href="#"><i class="fa fa-linkedin"></i></a>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</section>
+
+
